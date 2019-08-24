7ce48249f0ac1ea7c188814b8ccdb1ccb9752bfe
Ricardo Korn Moreira <ricardo.k.moreira@gmail.com>
2019-08-24 10:19:14 +0100

    Initial Commit

---
+++ ctwTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ctwTest
+//
+//  Created by Ricardo Moreira on 24/08/2019.
+//  Copyright © 2019 RKM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
